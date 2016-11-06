b82ba2e0145937ec89d72581f0dc04f13fac26e2
neldevfull <nfrugeri@gmail.com>
2016-11-06 21:11:57 +0000

    Improvement predict and expect of predict

--- machine_learning/farm_robot.py
+++ machine_learning/farm_robot.py
@@ -7,8 +7,6 @@ Data used for classification
 There are an array containing the values 1 or 0
 meaning: fat ? short leg ? bark ?
 
-result is 1 for pig and -1 for dog
-
 """
 
 from sklearn.naive_bayes import MultinomialNB
@@ -24,18 +22,31 @@ def main():
     dog3 = [0, 0, 1]
 
     data = [pig1, pig2, pig3, dog1, dog2, dog3]
-    results = ['pig', 'pig', 'pig', 'dog', 'dog', 'dog']
+    animals = ['pig', 'pig', 'pig', 'dog', 'dog', 'dog']
 
     sample1 = [1, 1, 1]
     sample2 = [1, 0, 0]
     sample3 = [1, 0, 1]
     sample4 = [0, 1, 0]
-    set_sample = [sample1, sample2, sample3, sample4]
 
     model = MultinomialNB()
-    model.fit(data, results)
+    model.fit(data, animals)
 
-    print(model.predict(set_sample))
+    set_sample = [sample1, sample2, sample3, sample4]
+    predict = model.predict(set_sample)
+
+    expect_results = ['dog', 'pig', 'dog', 'pig']
+    total = len(expect_results)
+    results = predict == expect_results
+
+    for result in results:
+        if not result:
+            total -= 1
+
+    total = total / 4 * 100
+
+    print('Results of predict is {}'.format(predict))
+    print('Predict is expected in {}% of assert'.format(total))
 
 
 if __name__ == '__main__':
